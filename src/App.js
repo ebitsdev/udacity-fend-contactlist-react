e5e5da51b6d99c45652eb1d8c1faa00e64874311
E. Bamba <ebamba@bitsnshine.com>
2018-09-30 09:59:39 -0400

    Completed the contact list with React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [
+        {
+          id: "ryan",
+          name: "Ryan Florence",
+          email: "ryan@reacttraining.com",
+          avatarURL: "http://localhost:5001/ryan.jpg"
+        },
+        {
+          id: "michael",
+          name: "Michael Jackson",
+          email: "michael@reacttraining.com",
+          avatarURL: "http://localhost:5001/michael.jpg"
+        },
+        {
+          id: "tyler",
+          name: "Tyler McGinnis",
+          email: "tyler@reacttraining.com",
+          avatarURL: "http://localhost:5001/tyler.jpg"
+        }
+      ]
+    };
+  }
+// We use this to remove the contact we are iterating over
+  removeContact = (contact) =>{
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+      }));
+
+  }
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
       </div>
-    )
+    );
   }
 }
 
---
+++ src/ListContacts.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+
+//When a component has only a render() method, then we can refactor it code to a stateless function like below
+
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: ''
+    }
+
+    // Create a method to invoque on value change
+
+    updateQuery = (query) => {
+        this.setState({query: query.trim() }) //trim() to remove any extra space
+    }
+
+    clearQuery = () => {
+        this.setState({ query: ''})
+    }
+    render(){
+        // We can unpack or destructure the contacts, onDeleteContact, and query like below, since they are coming from this.props
+        // This process of unpacking is called object destructuring.
+        const { contacts, onDeleteContact } = this.props;
+        const { query } = this.state;
+        let showingContacts;
+        if (query) {
+            const match = new RegExp(escapeRegExp(query), 'i') // 'i' is to ignore case in the regex
+            showingContacts = contacts.filter((contact) => match.test(contact.name))
+        } else {
+            showingContacts = contacts;
+        }
+
+        showingContacts.sort(sortBy('name'));
+        return (
+            <div className='list-contacts'>
+            {/* {JSON.stringify(this.state)} */}
+            <div className='list-contacts-top'>
+            <input className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={query }
+            onChange={(event) => this.updateQuery(event.target.value)}
+            />
+            </div>
+            {showingContacts.length !== contacts.length && (
+                <div className='search-contacts'>
+                <span>Now showing {showingContacts.length} of { contacts.length} total</span>
+                <button onClick={this.clearQuery}>Show all</button>
+                </div>
+            )}
+            <ol className='contact-list'>
+            {/* Create a jx component */}
+            {showingContacts.map(contact => (
+                <li className='contact-list-item' key={contact.id}>
+                <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})`}}/>
+                <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+                </div>
+                <button onClick={()=> onDeleteContact(contact)} className='contact-remove'>Remove</button>
+                </li>
+            ))}
+            </ol>
+        </div>
+        );
+    }
+}
+
+/**
+ *
+QUESTION 3 OF 3
+Before we head into the next Lesson, let's check in on our knowledge of Controlled Components!
+Which of the following is true about Controlled Components? Please check all that apply:
+Each update to state has an associated handler function
+Form elements receive their current value via an attribute
+Form input values are generally stored in the component's state
+Event handlers for a controlled element update the component's state
+ */
+
+ListContacts.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+}
+export default ListContacts
