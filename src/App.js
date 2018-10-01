34234a843a95f29053f9d2b4e9b0ad4c5d2502d7
E. Bamba <ebamba@bitsnshine.com>
2018-10-01 13:21:21 -0400

    Merged all changes from contactlist branch to master

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
-import * as ContactsAPI from './utils/ContactsAPI'
-
-class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
-  }
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-=======
 import React, { Component } from "react";
 import { Route } from 'react-router-dom';
 import ListContacts from "./ListContacts";
@@ -56,7 +28,6 @@ componentDidMount(){
     ContactsAPI.create(contact).then(contact => {
       this.setState(state => ({
         // Return an object by concatenating the new contact
->>>>>>> contactlist
         contacts: state.contacts.concat([ contact ])
       }))
     })
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom'
-=======
 import { BrowserRouter} from 'react-router-dom';
->>>>>>> contactlist
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-<<<<<<< HEAD
-  <BrowserRouter><App /></BrowserRouter>,
-  document.getElementById('root')
-);
-=======
 <BrowserRouter><App /></BrowserRouter>
 , document.getElementById('root'));
->>>>>>> contactlist
 registerServiceWorker();
